cc352559c0a5f64eb660ab71eae8f3b749c70d54
aditya-raj2230 <aditya17025@gmail.com>
2024-04-18 15:45:23 +0530

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,74 +1,76 @@
-import { NextAuthOptions } from 'next-auth'
-import { UpstashRedisAdapter } from '@next-auth/upstash-redis-adapter'
-import { db } from './db'
+import { NextAuthOptions } from "next-auth";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { db } from "./db";
 import GoogleProvider from 'next-auth/providers/google'
-import { fetchRedis } from '@/helpers/redis'
+import { fetchRedis } from "@/helpers/redis";
 
-function getGoogleCredentials() {
-  const clientId = process.env.GOOGLE_CLIENT_ID
-  const clientSecret = process.env.GOOGLE_CLIENT_SECRET
 
-  if (!clientId || clientId.length === 0) {
-    throw new Error('Missing GOOGLE_CLIENT_ID')
-  }
+function getGoogleCredentials(){
+    const clientId = process.env.GOOGLE_CLIENT_ID
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET
 
-  if (!clientSecret || clientSecret.length === 0) {
-    throw new Error('Missing GOOGLE_CLIENT_SECRET')
-  }
-
-  return { clientId, clientSecret }
+    if(!clientId || clientId.length ===0){
+        throw new Error("missing GOOGLE_CLIEND_ID")
+    }
+    if(!clientSecret || clientId.length ===0){
+        throw new Error("missing GOOGLE_CLIEND_SECRET")
+    }
+    return { clientId,clientSecret}
 }
 
-export const authOptions: NextAuthOptions = {
-  adapter: UpstashRedisAdapter(db),
-  session: {
-    strategy: 'jwt',
-  },
+export const authOptions:NextAuthOptions={
+    adapter:UpstashRedisAdapter(db),
+    session:{
+        strategy:'jwt'
+    },
+    pages:{
+        signIn:'/login'
+    },
+    providers:[
+        GoogleProvider({
+            clientId: getGoogleCredentials().clientId,
+            clientSecret: getGoogleCredentials().clientSecret,
+        }),
 
-  pages: {
-    signIn: '/login',
-  },
-  providers: [
-    GoogleProvider({
-      clientId: getGoogleCredentials().clientId,
-      clientSecret: getGoogleCredentials().clientSecret,
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      const dbUserResult = (await fetchRedis('get', `user:${token.id}`)) as
-        | string
-        | null
+    ],
+    callbacks:{
+        async jwt({token,user}){
+            const dbUserResult =(await fetchRedis('get',`user:${token.id}`))as 
+            |string
+            |null
 
-      if (!dbUserResult) {
-        if (user) {
-          token.id = user!.id
+            if(!dbUserResult){
+                token.id = user!.id
+                return token
+            }
+            const dbUser=JSON.parse(dbUserResult) as User
+
+            return {
+                id:dbUser.id,
+                name:dbUser.name,
+                email:dbUser.email,
+                picture:dbUser.image
+            }
+
+        },
+        async session({session,token}){
+            if(token){
+                session.user.id=token.id
+                session.user.name = token.name
+                session.user.email= token.email
+                session.user.image = token.picture
+            }
+
+            return session
+        },
+        redirect(){
+            return '/dashboard'
         }
+    }
 
-        return token
-      }
 
-      const dbUser = JSON.parse(dbUserResult) as User
 
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-      }
-    },
-    async session({ session, token }) {
-      if (token) {
-        session.user.id = token.id
-        session.user.name = token.name
-        session.user.email = token.email
-        session.user.image = token.picture
-      }
 
-      return session
-    },
-    redirect() {
-      return '/dashboard'
-    },
-  },
+
+
 }
